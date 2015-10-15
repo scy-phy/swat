e8f6e74694da5763c0b6cb241b67bf6e8202b220
David I. Urbina <david.urbina@utdallas.edu>
2015-10-15 18:53:45 +0800

    Moved util to commons. Added real level readings to the detection module

--- swatcrawler/p1_level_model.py
+++ swatcrawler/p1_level_model.py
@@ -1,17 +1,20 @@
 from __future__ import print_function
-from scapy import all as scapy_all
 import ctypes
 import math
+import threading
+import commons.util
+
+from scapy import all as scapy_all
 
 import swat
 import scaling
 import filters
-import util
 
 valve = 0
 pump = 0
+rlevel = 0
 real_level = 0
-est_level = 0.333
+est_level = 0.831
 L = 0.1
 T = 0.005
 cusum = 0
@@ -43,22 +46,43 @@ def calculate_estimated():
     cusum += abs(real_level - est_level) - ALPHA
     cusum = max(cusum, 0)
 
-    print('{} {}'.format(valve, pump), end=' ')
-    print('{}'.format(abs(real_level - est_level)), end=' ')
-    print('{}'.format(cusum))
+    # print('{} {}'.format(valve, pump), end=' ')
+    print('{},{},{},{}'.format(rlevel, real_level, abs(real_level - est_level), cusum), end='')
     # if abs(real_level - est_level) > T:
     if cusum > T:
         cusum = 0
-        print('SWaTAssault detected!!!!')
+        print(', SWaTAssault detected!!!!')
+    else:
+        print('')
+
+
+def read_real_level(packet):
+    if swat.SWAT_P1_RIO_AI in packet:
+        l = ctypes.c_short(packet[swat.SWAT_P1_RIO_AI].level).value
+        slevel = ctypes.c_short(packet[swat.SWAT_P1_RIO_AI].level)
+        l = filters.scale(slevel.value, scaling.P1Level)
+        global rlevel
+        rlevel = l * 0.001
 
 
 def start():
-    thread = util.RepeatEvery(1, calculate_estimated)
+    # try:
+    t_sniff_eth0 = threading.Thread(target=scapy_all.sniff,
+                                    kwargs={'iface': 'eth0', 'store': 0,
+                                            'prn': lambda p: read_real_level(p)})
+    # t_sniff_eth1 = threading.Thread(target=scapy_all.sniff,
+    #                                        kwargs={'iface': 'eth1', 'store': 0,
+    #                                                'prn': lambda p: get_model_parameters(p)})
+    t_detection = commons.util.RepeatEvery(1, calculate_estimated)
     print("*** Starting detection")
-    thread.start()
+    t_detection.start()
+    t_sniff_eth0.start()
+    # t_sniff_eth1.start()
     scapy_all.sniff(iface='eth1', store=0, prn=lambda p: get_model_parameters(p))
-    thread.stop()
-    thread.join()
+    # except KeyboardInterrupt:
+    t_detection.stop()
+    t_detection.join()
+    t_sniff_eth0.join()
     print("*** Stopping detection")
 
 
@@ -68,3 +92,7 @@ def configure():
 
 def params():
     pass
+
+
+if __name__ == '__main__':
+    start()
