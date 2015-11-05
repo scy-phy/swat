784cf50c0ea729322b4b91fbcc5ffddb44ef3eee
David I. Urbina <david.urbina@utdallas.edu>
2015-11-05 15:51:11 +0800

    Created an water level attack for P1 that uses cusum.
    Created a cusum model for attack detection of water level in P1.

--- swatcrawler/p1_level_model.py
+++ swatcrawler/p1_level_model.py
@@ -1,35 +1,41 @@
-from __future__ import print_function
+from __future__ import print_function, division
 import ctypes
 import math
 import threading
-import commons.util
+import sys
+import signal
 
 from scapy import all as scapy_all
 
+import commons.util
 import swat
 import scaling
 import filters
 
 valve = 0
 pump = 0
-rlevel = 0
-real_level = 0
-est_level = 0.831
+
+olevel = 0.695
+rlevel = 0.695
+elevel = 0.695
+
 L = 0.1
-T = 0.005
+tau = 0.1
+alpha = 0.005
 cusum = 0
 
-ALPHA = 0.0005
 FLOW = 2.5  # Water flow in m^3/h
 DIA = 1.38  # Tank 1 diameter
+
+stop_sniffer = False
 
 
 def get_model_parameters(packet):
     if swat.SWAT_P1_RIO_AI in packet:
-        global real_level
+        global rlevel
         l = ctypes.c_short(packet[swat.SWAT_P1_RIO_AI].level)
         l = filters.scale(l.value, scaling.P1Level)
-        real_level = l * 0.001
+        rlevel = l * 0.001
 
     if swat.SWAT_P1_RIO_DI in packet:
         global valve, pump
@@ -37,19 +43,19 @@ def get_model_parameters(packet):
         pump = packet[swat.SWAT_P1_RIO_DI].pump1_run
 
 
-def calculate_estimated():
-    global valve, pump, real_level, est_level, cusum
+def calculate_cusum():
+    global valve, pump, rlevel, elevel, cusum
     u = FLOW / (3600 * math.pi * math.pow(DIA / 2, 2))  # Water volume change
     u *= valve - pump  # Direction of water volume change
-    est_level = est_level + u + L * (real_level - est_level)
+    elevel = elevel + u + L * (rlevel - elevel)
 
-    cusum += abs(real_level - est_level) - ALPHA
+    cusum += abs(rlevel - elevel) - alpha
     cusum = max(cusum, 0)
 
     # print('{} {}'.format(valve, pump), end=' ')
-    print('{},{},{},{}'.format(rlevel, real_level, abs(real_level - est_level), cusum), end='')
-    # if abs(real_level - est_level) > T:
-    if cusum > T:
+    print('{},{},{},{},{}'.format(olevel, rlevel, elevel, abs(rlevel - elevel), cusum), end='')
+    # if abs(rlevel - elevel) > T:
+    if cusum > tau:
         cusum = 0
         print(', SWaTAssault detected!!!!')
     else:
@@ -61,38 +67,66 @@ def read_real_level(packet):
         l = ctypes.c_short(packet[swat.SWAT_P1_RIO_AI].level).value
         slevel = ctypes.c_short(packet[swat.SWAT_P1_RIO_AI].level)
         l = filters.scale(slevel.value, scaling.P1Level)
-        global rlevel
-        rlevel = l * 0.001
+        global olevel
+        olevel = l * 0.001
+
+
+def stop(p):
+    global stop_sniffer
+    return stop_sniffer
 
 
 def start():
-    # try:
-    t_sniff_eth0 = threading.Thread(target=scapy_all.sniff,
-                                    kwargs={'iface': 'eth0', 'store': 0,
-                                            'prn': lambda p: read_real_level(p)})
-    # t_sniff_eth1 = threading.Thread(target=scapy_all.sniff,
-    #                                        kwargs={'iface': 'eth1', 'store': 0,
-    #                                                'prn': lambda p: get_model_parameters(p)})
-    t_detection = commons.util.RepeatEvery(1, calculate_estimated)
-    print("*** Starting detection")
-    t_detection.start()
-    t_sniff_eth0.start()
-    # t_sniff_eth1.start()
-    scapy_all.sniff(iface='eth1', store=0, prn=lambda p: get_model_parameters(p))
-    # except KeyboardInterrupt:
-    t_detection.stop()
-    t_detection.join()
-    t_sniff_eth0.join()
-    print("*** Stopping detection")
+    __setup()
+    try:
+        # Thread to read real value before injection
+        t_sniff_eth0 = threading.Thread(target=scapy_all.sniff,
+                                        kwargs={'iface': 'eth0', 'store': 0,
+                                                'prn': lambda p: read_real_level(p),
+                                                'stop_filter': lambda p: stop(p)})
+
+        # Thread to read model parameters after injection
+        t_sniff_eth1 = threading.Thread(target=scapy_all.sniff,
+                                        kwargs={'iface': 'eth1', 'store': 0,
+                                                'prn': lambda p: get_model_parameters(p),
+                                                'stop_filter': lambda p: stop(p)})
+
+        t_detection = commons.util.RepeatEvery(1, calculate_cusum)
+        print("*** Starting detection")
+        t_sniff_eth0.start()
+        t_sniff_eth1.start()
+        t_detection.start()
+        signal.pause()
+    except KeyboardInterrupt:
+        t_detection.stop()
+        t_detection.join()
+        __setdown()
+        t_sniff_eth0.join()
+        t_sniff_eth1.join()
+        print("*** Stopping detection")
 
 
 def configure():
-    pass
+    global L, alpha, tau
+    L = input('Set Estimation Gain (L): ')
+    alpha = input('Set Alpha (A): ')
+    tau = input('Set Threshold (T): ')
+    params()
 
 
 def params():
-    pass
+    print('L: {} A: {} T: {}'.format(L, alpha, tau))
+
+
+def __setup():
+    global stop_sniffer
+    stop_sniffer = False
+
+
+def __setdown():
+    global stop_sniffer
+    stop_sniffer = True
 
 
 if __name__ == '__main__':
-    start()
+    sys.exit(start())
