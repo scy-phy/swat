3a282e149eb53cbcc890d98802c202e84d7d143d
BernardYuan <bernardyuan1@gmail.com>
2015-12-20 13:58:57 +0800

    updated test.py

--- test.py
+++ test.py
@@ -24,35 +24,57 @@ import time
 # In the following code, we construct a new packet with method2, which could make your machine imitate
 # a ring supervisor, and take control of the ring
 # newpacket = l2.Ether(**a[30467][l2.Ether].fields)/l2.Dot1Q(**a[30467][l2.Dot1Q].fields)/DLR.DLR(**a[30467][DLR.DLR].fields)
-newpacket = l2.Ether(**a[0][l2.Ether].fields)/l2.Dot1Q(**a[0][l2.Dot1Q].fields)/DLR.DLR(**a[0][DLR.DLR].fields)
+beaconpacket = l2.Ether(**a[0][l2.Ether].fields)/l2.Dot1Q(**a[0][l2.Dot1Q].fields)/DLR.DLR(**a[0][DLR.DLR].fields)
+announcepacket = l2.Ether(**a[1937][l2.Ether].fields)/l2.Dot1Q(**a[1937][l2.Dot1Q].fields)/DLR.DLR(**a[1937][DLR.DLR].fields)
 # newpacket.payload.payload.fields["Node_Num"] = 1
 # print newpacket.payload.payload.fields["SignOnInfo"]
 # newpacket.payload.payload.fields["SignOnInfo"]["MACAddress"] = ["00:1d:9c:c8:03:e8"]
 # newpacket.payload.payload.fields["SignOnInfo"]["IPAddress"] = ["192.168.0.19"]
-newpacket.explicit = 1
-newpacket.payload.explicit = 1
-newpacket.fields["src"] = "00:1d:9c:c8:03:e8"
-# newpacket.fields["dst"] = ""
+beaconpacket.explicit = 1
+beaconpacket.payload.explicit = 1
+
+announcepacket.explicit=1
+announcepacket.payload.explicit=1
+
+beaconpacket.fields["src"] = "00:1d:9c:c8:03:e8"
+announcepacket.fields["src"] = "00:1d:9c:c8:03:e8"
 # newpacket.payload.payload.fields["Source_Port"] = 1
-newpacket.payload.payload.fields["Supervisor_Precedence"] = 255
-newpacket.payload.payload.fields["Reserved"]='\x00\x01\x02\x03\x04\x05\x06\x07\x00\x01\x02\x03\x04\x05\x06\x07\x00\x01\x02\x03'
-newpacket.payload.payload.fields["Sequence_ID"] = 0
-newpacket.payload.payload.fields["Source_IP"] = '192.168.0.19'
+beaconpacket.payload.payload.fields["Supervisor_Precedence"] = 255
+beaconpacket.payload.payload.fields["Reserved"]='\x00\x01\x02\x03\x04\x05\x06\x07\x00\x01\x02\x03\x04\x05\x06\x07\x00\x01\x02\x03'
+beaconpacket.payload.payload.fields["Sequence_ID"] = 0
+beaconpacket.payload.payload.fields["Source_IP"] = '192.168.0.19'
+beaconpacket.payload.payload.fields["Beacon_Timeout"] = 1960
+beaconpacket.payload.payload.fields["Beacon_Interval"] = 400
 
+announcepacket.payload.payload.fields["Sequence_ID"] = 0
+announcepacket.payload.payload.fields["Source_IP"] = '192.168.0.19'
 # interval = newpacket.payload.payload.fields["Beacon_Interval"]
-start = time.clock()
-# print str(newpacket)
-# print all.hexdump(newpacket)
-all.sendp(str(newpacket),iface='en5')
-all.sendp(str(newpacket),iface='en4')
+beaconstart = time.clock()
+announcestart = time.clock()
 
+announcepacket.payload.payload.fields["Ring_State"] = 2
+beaconpacket.payload.payload.fields["Ring_State"] = 2
+for i in range(10) :
+    all.sendp(str(announcepacket),iface='en5')
+    # all.sendp(str(announcepacket),iface='en4')
+    all.sendp(str(beaconpacket),iface='en5')
+    all.sendp(str(beaconpacket),iface='en4')
+
+announcepacket.payload.payload.fields["Ring_State"] = 1
+beaconpacket.payload.payload.fields["Ring_State"] = 1
 while True:
-    if time.clock() - start > 400*1.0/1000000:
-        start = time.clock()
+    if time.clock() - beaconstart > 400*1.0/1000000:
+        beaconstart = time.clock()
     # print str(newpacket)
-        newpacket.payload.payload.fields["Sequence_ID"] = newpacket.payload.payload.fields["Sequence_ID"]+1
-        newpacket.show()
-        all.sendp(str(newpacket),iface='en5')
-        all.sendp(str(newpacket),iface='en4')
-
+        beaconpacket.payload.payload.fields["Sequence_ID"] = beaconpacket.payload.payload.fields["Sequence_ID"]+1
+        beaconpacket.show()
+        beaconpacket.payload.payload.fields["Reserved"]="\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        all.sendp(str(beaconpacket),iface='en5')
+        beaconpacket.payload.payload.fields["Reserved"]="\x01\x02\x03\x04\x05\x01\x02\x03\x04\x05\x01\x02\x03\x04\x05\x01\x02\x03\x04\x05"
+        all.sendp(str(beaconpacket),iface='en4')
+    if time.clock() - announcestart > 1:
+        announcestart = time.clock()
+        announcepacket.payload.payload.fields["Sequence_ID"] += 1
+        all.sendp(str(announcepacket),iface='en5')
+        # all.sendp(str(announcepacket),iface='en4')
 
